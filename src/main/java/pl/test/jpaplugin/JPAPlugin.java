38564486fa121a7a407c8ea8000d19065d60ec43
Hubert Rybicki <hubert.rybicki.hr@gmail.com>
2023-02-19 13:05:58 +0100

    Dodanie funkcji do PlayerService

--- src/main/java/pl/test/jpaplugin/JPAPlugin.java
+++ src/main/java/pl/test/jpaplugin/JPAPlugin.java
@@ -2,7 +2,12 @@ package pl.test.jpaplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import pl.test.jpaplugin.entity.Player;
+import pl.test.jpaplugin.service.PlayerService;
 
+import java.util.List;
+
+@SuppressWarnings("unused")
 public final class JPAPlugin extends JavaPlugin {
     public static JPAPlugin plugin;
     private static AnnotationConfigApplicationContext applicationContext;
@@ -13,10 +18,18 @@ public final class JPAPlugin extends JavaPlugin {
         applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.scan("pl.test.jpaplugin");
         applicationContext.refresh();
+
+
+        PlayerService playerService = (PlayerService) getApplicationContext().getBean("playerService");
+        List<Player> playerList = playerService.findAll();
     }
 
     @Override
     public void onDisable() {
         applicationContext.close();
+    }
+
+    public AnnotationConfigApplicationContext getApplicationContext(){
+        return applicationContext;
     }
 }
--- src/main/java/pl/test/jpaplugin/service/PlayerService.java
+++ src/main/java/pl/test/jpaplugin/service/PlayerService.java
@@ -5,11 +5,16 @@ import org.springframework.stereotype.Service;
 import pl.test.jpaplugin.entity.Player;
 import pl.test.jpaplugin.repository.PlayerRepository;
 
+import java.util.List;
+
 @Service("playerService")
 @RequiredArgsConstructor
 public class PlayerService {
     private final PlayerRepository playerRepository;
     public void addPlayer(Player player){
         playerRepository.save(player);
+    }
+    public List<Player> findAll(){
+        return playerRepository.findAll();
     }
 }
